e5b0310891e4c0b8f16dc0a1d62c0a4088e9c6d0
aaron.j <aaron.j@kakaocorp.com>
2023-01-09 10:35:32 +0900

    refactor: 10줄 이상 메서드 분리

--- src/main/java/controller/LottoController.java
+++ src/main/java/controller/LottoController.java
@@ -13,13 +13,22 @@ import java.util.stream.Collectors;
 public class LottoController {
 
     public static void run() {
-        String totalIssueCost = LottoInputView.inputAmount();
-        LottoValidator.isValidAmount(totalIssueCost);
+        String totalIssueCost = inputAndValidAmount();
 
         Lottos manualIssueLottos = manualIssueProcess();
         Lottos autoIssueLottos = autoIssueProcess(Integer.parseInt(totalIssueCost), LottoPrice.LOTTO_NORMAL_PRICE.get() * manualIssueLottos.getLottoList().size());
         LottoOutputView.printLottos(manualIssueLottos, autoIssueLottos);
 
+        mergeLottos(manualIssueLottos, autoIssueLottos);
+    }
+
+    private static String inputAndValidAmount() {
+        String totalIssueCost = LottoInputView.inputAmount();
+        LottoValidator.isValidAmount(totalIssueCost);
+        return totalIssueCost;
+    }
+
+    private static void mergeLottos(final Lottos manualIssueLottos, final Lottos autoIssueLottos) {
         Lottos lottos = LottoFactory.createLottos(autoIssueLottos, manualIssueLottos);
         resultProcess(lottos);
     }
@@ -29,8 +38,7 @@ public class LottoController {
     }
 
     private static Lottos manualIssueProcess() {
-        String manualCount = LottoInputView.inputManualCount();
-        LottoValidator.isValidManualCount(manualCount);
+        String manualCount = inputAndValidManualCount();
         List<String> inputManualLottoNumbers = LottoInputView.inputManualLottoNumbers(Integer.parseInt(manualCount));
         inputManualLottoNumbers.forEach(LottoValidator::isValidSixNumbers);
 
@@ -41,17 +49,33 @@ public class LottoController {
                 .collect(Collectors.toList()));
     }
 
-    private static void resultProcess(final Lottos lottos) {
-        String inputWinNumbers = LottoInputView.inputWinNumbers();
-        LottoValidator.isValidSixNumbers(inputWinNumbers);
-        Set<LottoNumber> lottoNumbers = Parser.parsingLottoNumbers(inputWinNumbers);
+    private static String inputAndValidManualCount() {
+        String manualCount = LottoInputView.inputManualCount();
+        LottoValidator.isValidManualCount(manualCount);
+        return manualCount;
+    }
 
-        String inputBonusNumber = LottoInputView.inputBonusNumber();
-        LottoValidator.isValidLottoNumber(inputBonusNumber);
+    private static void resultProcess(final Lottos lottos) {
+        Set<LottoNumber> lottoNumbers = inputAndValidWinNumbers();
+
+        String inputBonusNumber = inputAndValidBonusNumber();
 
         LottoNumber bonusLottoNumber = LottoNumber.getLottoNumber(Integer.parseInt(inputBonusNumber));
         WinLottoNumbers winLottoNumbers = new WinLottoNumbers(lottoNumbers, bonusLottoNumber);
 
         LottoOutputView.printResult(lottos, winLottoNumbers);
+    }
+
+    private static String inputAndValidBonusNumber() {
+        String inputBonusNumber = LottoInputView.inputBonusNumber();
+        LottoValidator.isValidLottoNumber(inputBonusNumber);
+        return inputBonusNumber;
+    }
+
+    private static Set<LottoNumber> inputAndValidWinNumbers() {
+        String inputWinNumbers = LottoInputView.inputWinNumbers();
+        LottoValidator.isValidSixNumbers(inputWinNumbers);
+        Set<LottoNumber> lottoNumbers = Parser.parsingLottoNumbers(inputWinNumbers);
+        return lottoNumbers;
     }
 }
