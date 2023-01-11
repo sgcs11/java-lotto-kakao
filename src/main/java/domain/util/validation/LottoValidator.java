f853ab35c65f2ac1384735a67fa478bdaf6e8932
aaron.j <aaron.j@kakaocorp.com>
2023-01-11 12:01:23 +0900

    refactor: isValidNumber에서 int 변환값을 반환하게 변경

--- src/main/java/domain/util/validation/LottoValidator.java
+++ src/main/java/domain/util/validation/LottoValidator.java
@@ -19,32 +19,32 @@ public class LottoValidator {
     }
 
     public static void isValidLottoNumber(String lottoNumber) {
-        isValidNumber(lottoNumber);
+        int validLottoNumber = isValidNumber(lottoNumber);
 
-        if (Integer.parseInt(lottoNumber) < 1 || Integer.parseInt(lottoNumber) > 45) {
+        if (validLottoNumber < 1 || validLottoNumber > 45) {
             throw new RuntimeException("유효한 번호가 입력되지 않았습니다.(유효한 번호: 1 ~ 45)");
         }
     }
 
     public static void isValidAmount(String amount) {
-        isValidNumber(amount);
+        int validAmount = isValidNumber(amount);
 
-        if (Integer.parseInt(amount) < 0 || Integer.parseInt(amount) % 1000 != 0) {
+        if (validAmount < 0 || validAmount % 1000 != 0) {
             throw new RuntimeException("금액은 0보다 큰 1000의 배수여야 합니다.");
         }
     }
 
     public static void isValidManualCount(String manualCount) {
-        isValidNumber(manualCount);
+        int validmanualCount = isValidNumber(manualCount);
 
-        if(Integer.parseInt(manualCount) < 0) {
+        if(validmanualCount < 0) {
             throw new RuntimeException("수동으로 구매할 로또 수가 음수입니다.");
         }
     }
 
-    public static void isValidNumber(String input) {
+    public static int isValidNumber(String input) {
         try {
-            Integer.parseInt(input);
+            return Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new RuntimeException("입력이 정수 형태가 아닙니다.");
         }
